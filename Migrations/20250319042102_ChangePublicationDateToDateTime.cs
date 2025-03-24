1741507c43393c2f9efda81724daf250df44bbaf
Sowmyabale <r988m564@wichita.edu>
2025-03-24 15:28:17 -0500

    Added Migrations

---
+++ Migrations/20250319042102_ChangePublicationDateToDateTime.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Research_Assistant.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePublicationDateToDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExtractedText",
+                table: "Papers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublicationDate",
+                table: "Papers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UploadDate",
+                table: "Papers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UploadDate",
+                table: "Papers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublicationDate",
+                table: "Papers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExtractedText",
+                table: "Papers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Migrations/20250320041202_AddExtractedTextToPapers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Research_Assistant.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExtractedTextToPapers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExtractedText",
+                table: "Papers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExtractedText",
+                table: "Papers");
+        }
+    }
+}
